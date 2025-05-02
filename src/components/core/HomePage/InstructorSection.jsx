c1a4bc1657190e7a43e48d494cdbc4a82072f361
nikpatil7 <nikhilpatil4714@gmail.com>
2025-05-03 01:53:19 +0530

    Make InstructorSection responsive

--- src/components/core/HomePage/InstructorSection.jsx
+++ src/components/core/HomePage/InstructorSection.jsx
@@ -6,38 +6,38 @@ import { FaArrowRight } from 'react-icons/fa'
 
 const InstructorSection = () => {
   return (
-    <div className='mt-16'>
+    <div className='mt-16 sm:mt-24 lg:mt-32 px-4 sm:px-6 lg:px-8'>
 
-      <div className="flex flex-row gap-20 items-center">
+      <div className="flex flex-col lg:flex-row gap-10 lg:gap-20 items-center">
 
-        <div className="w-[50%] ">
+        {/* Instructor Image */}
+        <div className="w-full lg:w-[50%] max-w-[500px] lg:max-w-none mx-auto">
           <img
             src={Instructor}
-            alt="InstructorImage"
-            className="shadow-white"
+            alt="Instructor teaching online"
+            className="shadow-white w-full h-auto object-contain"
           />
         </div>
 
-        <div className='w-[50%] flex flex-col gap-10'>
-            <div className='text-4xl font-semobold w-[50%]'>
+        {/* Content Section */}
+        <div className='w-full lg:w-[50%] flex flex-col gap-6 sm:gap-8 lg:gap-10'>
+            <div className='text-3xl sm:text-4xl font-semibold w-full lg:w-[80%] text-center lg:text-left'>
                 Become an
                 <HighlightText text={"Instructor"} />
             </div>
 
-            <p className='font-medium text-[16px] w-[80%] text-richblack-300'>
-            Instructors from around the world teach millions of students on StudyNotion. We provide the tools and skills to teach what you love.
+            <p className='font-medium text-sm sm:text-base lg:text-[16px] w-full lg:w-[80%] text-richblack-300 text-center lg:text-left'>
+              Instructors from around the world teach millions of students on StudyNotion. We provide the tools and skills to teach what you love.
             </p>
 
-            <div className='w-fit'>
+            <div className='w-fit mx-auto lg:mx-0'>
                 <CTAButton active={true} linkto={"/signup"}>
                     <div className='flex flex-row gap-2 items-center'>
-                        Start Learning Today
-                        <FaArrowRight />
+                        Start Teaching Today
+                        <FaArrowRight className="text-xs sm:text-sm" />
                     </div>
                 </CTAButton>
             </div>
-
-
         </div>
       </div>
     </div>
