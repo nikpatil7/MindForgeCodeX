b1136f03626d864b393ce63f4e732d4dbfb01e7f
nikpatil7 <nikhilpatil4714@gmail.com>
2025-07-29 00:30:23 +0530

    integrate Razorpay Payment Gateway

--- server/routes/Payments.js
+++ server/routes/Payments.js
@@ -1,11 +1,33 @@
+// // Import the required modules
+// const express = require("express")
+// const router = express.Router()
+
+// const { capturePayment, verifySignature } = require("../controllers/Payments")
+// const { auth, isInstructor, isStudent, isAdmin } = require("../middlewares/auth")
+
+// router.post("/capturePayment", auth, isStudent, capturePayment)
+// router.post("/verifySignature", verifySignature)
+
+// module.exports = router
+
 // Import the required modules
 const express = require("express")
 const router = express.Router()
-
-const { capturePayment, verifySignature } = require("../controllers/Payments")
+const {
+  capturePayment,
+  // verifySignature,
+  verifyPayment,
+  sendPaymentSuccessEmail,
+} = require("../controllers/Payments")
 const { auth, isInstructor, isStudent, isAdmin } = require("../middlewares/auth")
-
 router.post("/capturePayment", auth, isStudent, capturePayment)
-router.post("/verifySignature", verifySignature)
+router.post("/verifyPayment", auth, isStudent, verifyPayment)
+router.post(
+  "/sendPaymentSuccessEmail",
+  auth,
+  isStudent,
+  sendPaymentSuccessEmail
+)
+// router.post("/verifySignature", verifySignature)
 
 module.exports = router
